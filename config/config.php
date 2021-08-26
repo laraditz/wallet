8bcd67d3b0100159033e7db78a10384f46d9d118
Raditz Farhan <raditzfarhan@gmail.com>
2021-08-26 14:11:32 +0800

    Fix bug in config

--- config/config.php
+++ config/config.php
@@ -8,7 +8,7 @@ return [
     'tx_types' => env('WALLET_TX_TYPES', null),
     'wallet_type' => [
         'name' => env('WALLET_TYPE_DEFAULT_NAME', 'Default'),
-        'slug' => env('WALLET_TYPE_DEFAULT_NAME', 'default'),
+        'slug' => env('WALLET_TYPE_DEFAULT_SLUG', 'default'),
         'description' => env('WALLET_TYPE_DEFAULT_DESCRIPTION', 'The default wallet'),
         'currency_code' => env('WALLET_TYPE_DEFAULT_CURRENCY', 'MYR'),
     ],
