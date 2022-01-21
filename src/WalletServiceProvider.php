74b2e1c7b98ff26369735f52131ffa66b9a6433b
Raditz Farhan <raditzfarhan@gmail.com>
2022-01-22 00:51:14 +0800

    Check config_path function to support lumen

--- src/WalletServiceProvider.php
+++ src/WalletServiceProvider.php
@@ -20,9 +20,12 @@ class WalletServiceProvider extends ServiceProvider
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../config/config.php' => config_path('wallet.php'),
-            ], 'laraditz-wallet-config');
+
+            if (function_exists('config_path')) {
+                $this->publishes([
+                    __DIR__ . '/../config/config.php' => config_path('wallet.php'),
+                ], 'laraditz-wallet-config');
+            }
 
             // Publishing the views.
             /*$this->publishes([
